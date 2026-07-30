a89f36087f10eaede34ac85b754f89cd78b41b11
Null <tomtofficer@gmail.com>
2026-07-30 16:06:44 -0600

    Add FAT and modified VBR files

--- fat32/fat.h
+++ fat32/fat.h
@@ -1,44 +1,35 @@
-#ifndef FAT_H
-#define FAT_H
+#ifndef FAT32_FAT_H
+#define FAT32_FAT_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#include "vbr.h"
+#include "../vbr/vbr.h"
 
-#define FAT32_CLUSTER_FREE       0x00000000
-#define FAT32_CLUSTER_BAD        0x0FFFFFF7
-#define FAT32_CLUSTER_EOC        0x0FFFFFF8
+/* FAT32 entries use only their low 28 bits. */
+#define FAT32_CLUSTER_MASK       0x0FFFFFFFU
+#define FAT32_CLUSTER_FREE       0x00000000U
+#define FAT32_CLUSTER_BAD        0x0FFFFFF7U
+#define FAT32_CLUSTER_EOC        0x0FFFFFF8U
+#define FAT32_CLUSTER_EOC_LAST   0x0FFFFFFFU
 
-bool FATReadEntry(
-    const FAT32BootSector *Boot,
-    uint32_t PartitionStartLBA,
-    uint32_t Cluster,
-    uint32_t *NextCluster
-);
+/*
+ * Read or update an entry in the file allocation table.  PartitionStartLBA
+ * is the LBA of the FAT32 volume's boot sector, not the disk's MBR LBA.
+ *
+ * FATFindFreeCluster returns 0 when no free data cluster is available.
+ */
+bool FATReadEntry(const FAT32BootSector *Boot, uint32_t PartitionStartLBA,
+                  uint32_t Cluster, uint32_t *NextCluster);
 
-bool FATWriteEntry(
-    const FAT32BootSector *Boot,
-    uint32_t PartitionStartLBA,
-    uint32_t Cluster,
-    uint32_t Value
-);
+bool FATWriteEntry(const FAT32BootSector *Boot, uint32_t PartitionStartLBA,
+                   uint32_t Cluster, uint32_t Value);
 
-bool FATIsEndOfChain(
-    uint32_t Cluster
-);
+bool FATIsEndOfChain(uint32_t Cluster);
+bool FATIsFreeCluster(uint32_t Cluster);
+bool FATIsBadCluster(uint32_t Cluster);
 
-bool FATIsFreeCluster(
-    uint32_t Cluster
-);
-
-bool FATIsBadCluster(
-    uint32_t Cluster
-);
-
-uint32_t FATFindFreeCluster(
-    const FAT32BootSector *Boot,
-    uint32_t PartitionStartLBA
-);
+uint32_t FATFindFreeCluster(const FAT32BootSector *Boot,
+                            uint32_t PartitionStartLBA);
 
 #endif
